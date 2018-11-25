0355737368c0fa52d3da6b7043ecfcbd1af7343d
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-25 17:59:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(dwarves_list)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |calls|
+  planeteer_calls.map do |calls|
       puts "#{calls.capitalize}!"
     end 
 end
